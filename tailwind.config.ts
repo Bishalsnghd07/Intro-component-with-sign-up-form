d009f6be497184f5f53cc92b78182ff3c2b34ff8
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-24 17:56:16 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      'poppins': ['poppins', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        "inner-button": "inset 0px -4px 0px rgba(0, 0, 0, 0.0908818)",
+        card: "0px 8px 0px rgba(0, 0, 0, 0.14688)",
       },
     },
   },
