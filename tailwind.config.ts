1d2c9e7c48c065a591c61aca898d960af2985d96
Bishal Singh Deo <88236153+Bishalsnghd07@users.noreply.github.com>
2024-03-01 16:48:15 +0530

    Update tailwind.config.ts
    update font-family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,6 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      'poppins': ['poppins', 'sans-serif']
-    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
